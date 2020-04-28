575222aa0a5c522ecdb38c67d31d98652c45c6cc
Ana Pineda <anamichellepd@gmail.com>
2020-04-28 16:06:54 -0700

    added note section

--- public/app.js
+++ public/app.js
@@ -32,9 +32,46 @@ $(".clear-btn").on("click", function () {
 //Save the article button
 $(document).on("click", "#saveButton", function () {
   //grabbing the ID associated with the article
+  // Empty the notes from the note section
+  $("#notes").empty();
 
   var thisId = $(this).attr("data-id");
-  $(this).parentsUntil(thisId).hide();
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append(
+        "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
+      );
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+// When you click the savenote button
+$(document).on("click", "#savenote", function () {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
 
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
@@ -52,6 +89,7 @@ $(document).on("click", "#saveButton", function () {
       // Log the response
       console.log(data);
       // Empty the notes section
+      $("#notes").empty();
     });
 
   // Also, remove the values entered in the input and textarea for note entry
