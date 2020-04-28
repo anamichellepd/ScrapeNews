f307dfd8602136ea39906b93214c833aa773029d
Ana Pineda <anamichellepd@gmail.com>
2020-04-27 21:54:14 -0700

    worked on the comment button but can't get it to work yet

--- public/app.js
+++ public/app.js
@@ -1,11 +1,13 @@
 //Grab the articles as a JSON
 $.getJSON("/articles", function (data) {
+  $("#articles").empty();
   for (let i = 0; i < 10; i++) {
     $("#articles").append(
-      "<div class='card'>",
-      `<a href=https://news.google.com/${data[i].link}<h5 data-id='${data[i]._id}'class='card-header'>${data[i].title}</h5></a>`,
+      `<div id='card' data-id='${data[i]._id}'>"`,
+      `<a href=https://news.google.com/${data[i].link}>
+          <h5 data-id='${data[i]._id}'class='card-header'>${data[i].title}</h5></a>`,
       "<div class='card-body'> ",
-      `<a href='#' id='saveButton' data-id='${data[i]._id}' class='comment-button btn btn-primary'>Save this article</a>`,
+      `<button id='saveButton' data-id='${data[i]._id}' class='comment-button btn btn-primary'>Save this article</button>`,
       "<h5 class='card-title'>" + "Source: " + data[i].source + "</h5>",
       "<br>",
       "</div>",
@@ -22,10 +24,17 @@ $(".scrape-btn").on("click", function () {
   });
 });
 
+//Clear articles button
+$(".clear-btn").on("click", function () {
+  $("#articles").empty();
+});
+
 //Save the article button
 $(document).on("click", "#saveButton", function () {
   //grabbing the ID associated with the article
+
   var thisId = $(this).attr("data-id");
+  $(this).parentsUntil(thisId).hide();
 
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
@@ -43,7 +52,6 @@ $(document).on("click", "#saveButton", function () {
       // Log the response
       console.log(data);
       // Empty the notes section
-      $("#notes").empty();
     });
 
   // Also, remove the values entered in the input and textarea for note entry
